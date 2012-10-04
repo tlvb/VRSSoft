8c8e4fe08ba2f13404e6d7b4ca7cc630148542ef
Leo Bärring <leo.barring@gmail.com>
2012-10-05 00:45:23 +0200

    python port of shell player begun
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

--- lighttools.py
+++ lighttools.py
@@ -1,3 +1,15 @@
+class Device:
+	def __init__(self, devfile):
+		self.wfd = open(devfile, 'w')
+		self.rfd = open(devfile, 'r')
+	def write(self, stuff):
+		self.wfd.write(stuff)
+	def close(self):
+		if not self.wfd.closed:
+			self.wfd.close()
+		if not self.rfd.closed:
+			self.rfd.close()
+
 def lg2(x):
 	y = 0
 	while not x & 1:
@@ -24,19 +36,14 @@ def mk_command(ctype, start=None, length=None):
 	else:
 		return 255
 
-
 def mk_chain(new, start, length):
-	return ["change({}, {}, {})".format(start, length, mk_command('channel', start, length))] + new[start:start+length]
+	return chr(mk_command('channel', start, length)) + ''.join(chr(x) for x in new[start:start+length])
 
 def find_opt_comchain(changeset, new, start, length):
 	if length > 1:
-		print("> {} {}".format(start,length))
 		comchain0 = mk_chain(new, start, length)
 		comchain1 = find_opt_comchain(changeset, new, start, length>>1)
 		comchain2 = find_opt_comchain(changeset, new, start+(length>>1), length>>1)
-		print("cc0: ",comchain0)
-		print("cc1: ",comchain1)
-		print("cc2: ",comchain2)
 		if len(comchain0) < len(comchain1) + len(comchain2):
 			return comchain0
 		else:
@@ -45,35 +52,19 @@ def find_opt_comchain(changeset, new, start, length):
 		if changeset[start] != None:
 			return mk_chain(new, start, length) 
 		else:
-			return []
+			return ''
+
+def mk_comchain(old, new):
+	changeset = mk_changeset(old, new)
+	return find_opt_comchain(changeset, new, 0, len(changeset))
 
 if __name__ == '__main__':
 
-	import sys
+	o = [1,2,3,4,5,6,7,8]
+	n = [7,3,2,4,5,6,9,8]
+	c = mk_comchain(o, n)
+	print(repr(c))
 
-	print("#ifndef {}".format(sys.argv[1]))
-	print("#define {}".format(sys.argv[1]))
-	print("/* automatically generated from 'python {} {}' */".format(sys.argv[0], sys.argv[1]))
-
-	for i in range(64):
-		print("#define PR_G1_{} {}".format(i, mk_command('channel', i, 1)))
-	for i in range(32):
-		print("#define PR_G2_{} {}".format(i, mk_command('channel', i, 2)))
-	for i in range(16):
-		print("#define PR_G4_{} {}".format(i, mk_command('channel', i, 4)))
-	for i in range(8):
-		print("#define PR_G8_{} {}".format(i, mk_command('channel', i, 8)))
-	for i in range(4):
-		print("#define PR_G16_{} {}".format(i, mk_command('channel', i, 16)))
-	for i in range(2):
-		print("#define PR_G32_{} {}".format(i, mk_command('channel', i, 32)))
-	for i in range(1):
-		print("#define PR_G64_{} {}".format(i, mk_command('channel', i, 64)))
-	print("#define PR_RESET {}".format(mk_command('reset')))
-	print("#define PR_LIMBO {}".format(mk_command('limbo')))
-	print("#define PR_OK {}".format(mk_command('ok')))
-
-	print("#endif")
 
 
 
